dd462869ca0b073173c23feefe9ddcc4be9e142a
Ben Wiklund <ben@daisyowl.com>
2023-04-22 12:48:59 -0700

    setup

--- MicroJson/Program.cs
+++ MicroJson/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Dyn {
+  public string? str;
+  public double? num;
+
+}
+public static class Mjson {
+  public static bool Parse(string str, int idx = 0)
+  {
+    
+  }
+}
